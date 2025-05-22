23c847e7974327d2e5c4548aba7e1c6fbb8deac0
syaakia <claraaativaani@gmail.com>
2025-05-22 15:02:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,8 @@ st.write(
     "kucingg"
 )
 st.image("IMG-20240515-WA0110.jpeg", width=200)
-#st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis sebuah Angka:", value=θ, step=1)
 
 if (angka % 2) ==  θ:
